85edab4ddf2fed0ba17dd19ebc1d21304f687729
diegobauto <diegobauto00@gmail.com>
2024-05-28 09:20:52 -0500

    feat: Variable de entorno notificaciones udistrital/planeacion_cliente/issues/1068

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,17 +6,18 @@ export const environment = {
     menuApps: false,
     appname: "PLANEACION",
     appMenu: "PLANEACION",
+
+    // Notificaciones
+    NOTIFICACION_MID_SERVICE: "https://autenticacion.portaloas.udistrital.edu.co/apioas/notificacion_mid/v1/",
+
     TOKEN: {
-      AUTORIZATION_URL:
-        "https://autenticacion.portaloas.udistrital.edu.co/oauth2/authorize",
+      AUTORIZATION_URL: "https://autenticacion.portaloas.udistrital.edu.co/oauth2/authorize",
       CLIENTE_ID: "e36v1MPQk2jbz9KM4SmKhk8Cyw0a",
       RESPONSE_TYPE: "id_token token",
       SCOPE: "openid email",
       REDIRECT_URL: "https://pruebaspsisgplanmf.portaloas.udistrital.edu.co",
-      SIGN_OUT_URL:
-        "https://autenticacion.portaloas.udistrital.edu.co/oidc/logout",
+      SIGN_OUT_URL: "https://autenticacion.portaloas.udistrital.edu.co/oidc/logout",
       SIGN_OUT_REDIRECT_URL: "https://pruebaspsisgplanmf.portaloas.udistrital.edu.co",
-      AUTENTICACION_MID:
-        "https://autenticacion.portaloas.udistrital.edu.co/apioas/autenticacion_mid/v1/token/userRol",
+      AUTENTICACION_MID: "https://autenticacion.portaloas.udistrital.edu.co/apioas/autenticacion_mid/v1/token/userRol",
     },
   };
